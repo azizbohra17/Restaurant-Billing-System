bebc82961b86200820b12243fcfe1a57e00f59a4
Aziz Bohra <47524749+azizbohra17@users.noreply.github.com>
2019-10-16 19:07:59 +0530

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,11 +1,12 @@
+//main class
+package my_mini_project;
 
 public class Driver 
 {
 	public static void main(String args[])
 	{
-		login l=new login();
-		l.lg();
-		display_menu disp=new display_menu();
-		disp.disp_menu();
+	    
+		login l=new login();  //Calling Login class
+		l.lg();            
 	}
 }
